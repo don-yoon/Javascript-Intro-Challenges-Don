a0cc81d7e37b1b4352c26d3d634e31e00dfa2a0f
Don Yoon <dongho.yoon@gmail.com>
2022-05-12 23:32:42 -0700

    quick preview with group

---
+++ functions-practice-studygroup.js
@@ -0,0 +1,13 @@
+
+//greeting()
+
+const greeting = () => {
+    return "Hello study group"
+}
+
+console.log(greeting())
+
+const multiplier = (num1, num2) => {
+    return num1 * num2
+  }
+  console.log(multiplier(5, 5))
